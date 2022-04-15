f94ce0201385b31c0d194ebb270b88b0911613f8
Jaffar Hussein <jaffhussein@icloud.com>
2022-04-15 15:30:33 +0200

    ft:clicks-updated the strike through to take effect on double clicks

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
+  constructor(private elem:ElementRef) { }
 
-  constructor(private elem:ElementRef) { 
-    this.elem.nativeElement.style.textDecoration = 'line-through';
+  @HostListener('dblclick') onDBClicks(){
+    this.textDeco('line-through')
+  }
+  @HostListener('click') onClicks(){
+    this.textDeco('none')
+  }
+  
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration = action;
   }
 
 }
