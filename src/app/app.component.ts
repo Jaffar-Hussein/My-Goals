cd739b114ad8f468db739111175e2b8ddfe94fc9
Jaffar Hussein <jaffhussein@icloud.com>
2022-04-15 06:25:04 +0200

    ft:ngClass - used them to create skeleton for the goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{Goal} from './goal'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,12 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // goal = "watch finding nemo"
-  goals:string[];
+  // goals:string[];
 
   // A constructor function defines the logic that should be executed 
   // once the class is instantiated
 
-  constructor(){
-    this.goals = ['watch finding nemo',"learn angular","learn flask","finish school","do my internship"]
-  }
+  // constructor(){
+  //   this.goals = ['watch finding nemo',"learn angular","learn flask","finish school","do my internship"]
+  // }
+  // Using a class as the skeleton for building things
+
+  goals:Goal[] = [
+    {id:1,name:'watch finding nemo'},
+    {id:2,name:"learn angular"},
+    {id:3,name:"learn flask"},
+    {id:4,name:"finish school"},
+    {id:5,name:"do my internship"},
+  ]
 }
